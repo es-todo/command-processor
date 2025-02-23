ceb83dffb595bb6dff3ed889f7cb981cac4d4d17
Abdulaziz Ghuloum <aghuloum@gmail.com>
2025-02-23 07:21:02 +0300

    drafting command rules

--- src/command-rules.ts
+++ src/command-rules.ts
@@ -5,20 +5,40 @@ import {
 
 import { type event_type } from "schemata/dist/generated/event_type";
 import { type object_type } from "schemata/dist/generated/object_type";
+import { parse_object_type } from "schemata/generated/object_type";
 
-type fetch_func<k> = object_type extends { type: k; value: infer v }
-  ? {
-      type: k;
-      id: string;
-      sk: (v: v) => command_outcome;
-      fk: () => command_outcome;
-    }
-  : never;
+type fetch_func<k> = {
+  type: k;
+  id: string;
+  sk: (v: object_val<k>) => command_outcome;
+  fk: () => command_outcome;
+};
 
 type fetch_desc = fetch_func<object_type["type"]>;
+type object_val<T> = object_type extends { type: T; value: infer v }
+  ? v
+  : never;
+
+function fetch<T extends object_type["type"]>(
+  type: T,
+  id: string,
+  sk: (val: object_val<T>) => command_outcome
+): command_outcome {
+  return {
+    type: "fetch",
+    desc: {
+      type,
+      id,
+      sk: (v) => sk(v as any),
+      fk: () => ({ type: "failed", reason: "not found" }),
+    },
+  };
+}
 
 type command_outcome =
   | { type: "fetch"; desc: fetch_desc }
+  | terminal_command_outcome;
+type terminal_command_outcome =
   | { type: "succeeded"; events: event_type[] }
   | { type: "failed"; reason: string };
 
@@ -61,3 +81,24 @@ const command_rules: command_rules = {
 const e = parse_command_type({ type: "register" });
 const insp = command_rules[e.type];
 const out = insp(({ handler }) => handler(e.value as any));
+
+function fetch_object(type: string, id: string) {}
+
+function f(out: command_outcome): terminal_command_outcome {
+  switch (out.type) {
+    case "fetch": {
+      const { type, id, sk, fk } = out.desc;
+      const o = parse_object_type(fetch_object(type, id));
+      if (o.type === type) {
+        return f(sk(o.value));
+      } else {
+        return f(fk());
+      }
+    }
+    case "succeeded":
+    case "failed":
+      return out;
+  }
+}
+
+f(out);
